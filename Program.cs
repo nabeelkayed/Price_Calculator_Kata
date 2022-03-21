135e85b4598b0db6e678690e548f60a55294e651
nabeelkayed <nabeelkayed25@gmail.com>
2022-03-21 12:08:21 +0200

    Tax

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Price_Calculator_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product newProduct = new Product("The Little Prince", 12345, 20.25);
+
+            Console.WriteLine("Please Enter the tax percentage:");
+            newProduct.Tax = Convert.ToDouble(Console.ReadLine());
+            newProduct.UpdatePriceAfterNewTax();
+
+            Console.WriteLine($"Product price reported as {newProduct.Price} before tax and {newProduct.PriceAfterTax} after {newProduct.Tax}% tax");
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Price_Calculator_Kata
+{
+    public class Product
+    {
+        public string Name { set; get; }
+        public int UPC { set; get; }
+        public double Price { set; get; }
+        public double Tax { set; get; }
+        public double PriceAfterTax { set; get; }
+
+        public Product(string Name, int UPC, double Price)
+        {
+            this.Name = Name;
+            this.UPC = UPC;
+            this.Price = Price;
+            Tax = 20;
+            PriceAfterTax = Math.Round(Price + (Price * Tax / 100), 2);
+
+        }
+
+        public void UpdatePriceAfterNewTax()
+        {
+            PriceAfterTax = Math.Round(Price + (Price * Tax / 100), 2);
+        }
+    }
+}
